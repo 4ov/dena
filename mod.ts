118c0c58244f491cd64496d58e05a81b19576dfa
4ov <4ov@rcon.link>
2021-07-11 16:53:26 +0200

    exported all classes in mod.ts

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
-export {
-    default 
-} from "./dena.ts"
+export { default } from "./dena.ts"
+export * as DenaTypes from './types.ts'
+export { default as Base } from './base.ts'
+export { default as Drive } from './drive.ts'
